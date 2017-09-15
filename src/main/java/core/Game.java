5c31f0822ebac84e33964f2d9e3739e470bf2598
Will Pearson <will.pearson@xtra.co.nz>
2017-09-15 20:14:30 +1200

    Updated Game with Input Handler functionality
    Overrides Processing's main input events and passes them to the
    InputHandler.

--- src/main/java/core/Game.java
+++ src/main/java/core/Game.java
@@ -1,9 +1,32 @@
 package core;
 
+import core.input.InputHandler;
 import processing.core.PApplet;
 
 /**
  * Created by Callum Li on 9/14/17.
  */
 public abstract class Game extends PApplet {
+
+    //----------------------Input Handler---------------------
+
+    @Override
+    public void mousePressed() {
+        InputHandler.setMousePressed(true, mouseButton);
+    }
+
+    @Override
+    public void mouseReleased() {
+        InputHandler.setMousePressed(false, mouseButton);
+    }
+
+    @Override
+    public void keyPressed() {
+        InputHandler.setKeyPressed(true, keyCode);
+    }
+
+    @Override
+    public void keyReleased() {
+        InputHandler.setKeyPressed(false, keyCode);
+    }
 }
