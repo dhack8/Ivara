6e4614c892c34cad04d97134673ecd4716e414bc
David Hack <dhack8@gmail.com>
2017-09-15 20:04:23 +1200

    Added constructors to the vector class.

--- src/main/java/maths/Vector.java
+++ src/main/java/maths/Vector.java
@@ -3,7 +3,26 @@ package maths;
 import processing.core.PVector;
 
 /**
- * Created by Callum Li on 9/15/17.
+ * Class to store 2D vectors with floats.
+ *
+ * @author David Hack
  */
 public class Vector extends PVector{
+
+    /**
+     * Creates a new Vector class from x y.
+     * @param x x location
+     * @param y y location
+     */
+    public Vector(float x, float y){
+        super(x,y);
+    }
+
+    /**
+     * Creates a new Vector class from another.
+     * @param other another vector
+     */
+    public Vector(Vector other){
+        super(other.x, other.y);
+    }
 }
