b08110bc9ddbc7fa4ce15aa71ebfec98e9154873
Alex <alexjfmitchell@gmail.com>
2017-09-15 21:41:38 +1200

    Created PlayerEntity

---
+++ src/main/java/pxljam/PlayerEntity.java
@@ -0,0 +1,19 @@
+package pxljam;
+
+import core.components.CollidierComponent;
+import core.components.ScriptComponent;
+import core.components.SpriteComponent;
+import core.scene.Entity;
+
+/**
+ * Created by Alex Mitchell on 15/09/2017.
+ */
+public class PlayerEntity extends Entity{
+
+    SpriteComponent sprite;
+    ScriptComponent script;
+    CollidierComponent collider;
+
+}
+
+
