741d1cc22552b79aa356cb51e8044e10022beea0
Alex <alexjfmitchell@gmail.com>
2017-09-16 13:49:42 +1200

    Added to ClassMapTesting

--- src/test/java/util/ClassMapTest.java
+++ src/test/java/util/ClassMapTest.java
@@ -21,6 +21,10 @@ public class ClassMapTest {
     }
 
     @Test
+    /**
+     * Tests that the map correctly adds an object and that the set of the object's type contains
+     * the object itself and that the length is equal to 1.
+     */
     public void testBasicAdd1(){
         String t1 = "test";
         Integer t2 = 1;
@@ -35,6 +39,9 @@ public class ClassMapTest {
     }
 
     @Test
+    /**
+     * Tests that the items added to the set are correctly returned with the get method.
+     */
     public void testBasicGet1(){
         int testSize = 4;
 
@@ -50,5 +57,37 @@ public class ClassMapTest {
             assert temp.contains(i);
         }
         assert temp.size() == testSize;
+    }
+
+    @Test
+    /**
+     * Tests that getting objects of different classes returns what it should.
+     */
+    public void testBasicGet2(){
+        int testSize = 4;
+
+        List<Integer> testValues1 = new ArrayList<>();
+        List<Character> testValues2 = new ArrayList<>();
+
+        for(int i = 0; i < testSize; i++){
+            Integer test = i;
+            Character test1 = (char) i;
+            testValues1.add(test);
+            testValues2.add(test1);
+            map.put(test);
+            map.put(test1);
+        }
+
+        Collection<Integer> temp1 = map.get(Integer.class);
+        for(Integer i : testValues1){
+            assert temp1.contains(i);
+        }
+        assert temp1.size() == testSize;
+
+        Collection<Character> temp2 = map.get(Character.class);
+        for(Character i : testValues2){
+            assert temp2.contains(i);
+        }
+        assert temp2.size() == testSize;
     }
 }
