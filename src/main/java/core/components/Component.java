ee25405f0dff4b81d875994679eda9bbe232f5ab
Callum Li <CallumL1009@gmail.com>
2017-09-16 12:01:07 +1200

    Added reference from components to parent entity

--- src/main/java/core/components/Component.java
+++ src/main/java/core/components/Component.java
@@ -1,8 +1,14 @@
 package core.components;
 
+import core.scene.Entity;
+
 /**
  * Created by Callum Li on 9/15/17.
  */
 public abstract class Component {
+    private final Entity entity;
 
+    public Component(Entity entity) {
+        this.entity = entity;
+    }
 }
