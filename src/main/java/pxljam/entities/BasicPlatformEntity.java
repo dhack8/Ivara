8028b3934ef50ad9ccb600541485c96970df78e9
Alex <alexjfmitchell@gmail.com>
2017-09-16 12:31:53 +1200

    Removed PlatformEntity, changed constructors for other entities

--- src/main/java/pxljam/entities/BasicPlatformEntity.java
+++ src/main/java/pxljam/entities/BasicPlatformEntity.java
@@ -2,6 +2,7 @@ package pxljam.entities;
 
 import core.components.ColliderComponent;
 import core.components.SpriteComponent;
+import core.scene.Entity;
 import maths.Vector;
 import physics.Collider;
 
@@ -9,9 +10,19 @@ import physics.Collider;
  * This class handles the basic platform entity
  * @author Alex Mitchell
  */
-public class BasicPlatformEntity extends PlatformEntity{
+public class BasicPlatformEntity extends Entity{
+    private SpriteComponent sprite;
+    private ColliderComponent collider;
 
-    public BasicPlatformEntity(Vector v, SpriteComponent s, ColliderComponent c){
-        super(v,s,c);
+    /**
+     * Creates a Basic Platform Entity at a specified position
+     * @param x The x position of the top-left corner of the platform
+     * @param y The y position of top-left corner of the platform
+     */
+    public BasicPlatformEntity(float x, float y){
+        super(new Vector(x,y));
+        Vector v = new Vector(x,y);
+        sprite = new SpriteComponent();
+        collider = new ColliderComponent();
     }
 }
--- src/main/java/pxljam/entities/PlayerEntity.java
+++ src/main/java/pxljam/entities/PlayerEntity.java
@@ -5,6 +5,7 @@ import core.components.ScriptComponent;
 import core.components.SpriteComponent;
 import core.scene.Entity;
 import maths.Vector;
+import pxljam.scripts.PlayerController;
 
 /**
  * This class handles the entity of the player within the game
@@ -22,11 +23,23 @@ public class PlayerEntity extends Entity{
      * @param script The controller of the player
      * @param collider The collision aspect of the player
      */
-    public PlayerEntity(Vector vector, SpriteComponent sprite, ScriptComponent script, ColliderComponent collider) {
+    public PlayerEntity(Vector vector, SpriteComponent sprite, ScriptComponent script, ColliderComponent collider) { //Todo decide whether this is a useful constructor
         super(vector);
         this.sprite = sprite;
         this.script = script;
         this.collider = collider;
+    }
+
+    /**
+     * Creates a PlayerEntity at a given position
+     * @param x The x position
+     * @param y The y position
+     */
+    public PlayerEntity(float x, float y){
+        super(new Vector(x,y));
+        this.sprite = new SpriteComponent();
+        this.script = new PlayerController(this);
+        this.collider = new ColliderComponent();
     }
 }
 
