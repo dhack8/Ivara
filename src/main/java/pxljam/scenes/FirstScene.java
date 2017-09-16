2d7184c65496face051f2a6efad298afccb6de16
Alex <alexjfmitchell@gmail.com>
2017-09-16 20:25:12 +1200

    Updated firstScene

--- src/main/java/pxljam/scenes/FirstScene.java
+++ src/main/java/pxljam/scenes/FirstScene.java
@@ -8,15 +8,22 @@ import pxljam.entities.PlayerEntity;
  * Our very first scene.
  *
  * @author David Hack
+ * @author Alex Mitchell
  */
 public class FirstScene extends Scene {
 
     public FirstScene() {
         addEntity(new PlayerEntity(2, 2));
         for(int i = 0; i < 4; i++){
-
             addEntity(new BasicBlockEntity(i,3, "grass-top"));
+        }
 
-        }      
+        addEntity(new BasicBlockEntity(6, 4, "dirt"));
+
+        for(int i = 8; i < 11; i++){
+            addEntity(new BasicBlockEntity(i,5, "grass-top"));
+        }
+
+
     }
 }
