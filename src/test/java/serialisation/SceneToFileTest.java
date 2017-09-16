81c7e205ce2e2eaa640ab31007752da66e5c91ba
James Magallanes <jamesmagallanes@gmail.com>
2017-09-16 17:36:37 +1200

    Test for SceneToFile

---
+++ src/test/java/serialisation/SceneToFileTest.java
@@ -0,0 +1,38 @@
+package serialisation;
+
+import core.scene.Entity;
+import core.scene.Scene;
+import maths.Vector;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by james on 16/09/2017.
+ */
+public class SceneToFileTest {
+
+    SceneToFile stf;
+    Scene scene;
+
+    @Before
+    public void setUp() throws Exception {
+        scene = new Scene(){};
+        stf = new SceneToFile(scene);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void write() throws Exception {
+        scene.addEntity(new Entity(new Vector(10, 12)){});
+        scene.addEntity(new Entity(new Vector(11, 12)){});
+        stf.write();
+    }
+
+}
