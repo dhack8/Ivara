38b6379692c20a1bd75a9772f54258a896329cf2
Will Pearson <will.pearson@xtra.co.nz>
2017-09-16 19:48:31 +1200

    Updated EntityContainer with functionality
    Can get all components of the given type and get all entities.

--- src/main/java/core/entity/EntityContainer.java
+++ src/main/java/core/entity/EntityContainer.java
@@ -2,6 +2,7 @@ package core.entity;
 
 import core.components.Component;
 import core.scene.Entity;
+import util.ClassMap;
 
 import java.util.Collection;
 import java.util.HashSet;
@@ -15,20 +16,28 @@ import java.util.HashSet;
 public class EntityContainer {
 
     Collection<Entity> entities = new HashSet<>();
+    ClassMap classMap = new ClassMap();
 
-    public boolean addEntity(Entity entity) {
-        return entities.add(entity);
+    public void addEntity(Entity entity) {
+        entities.add(entity);
+        for (Component component : entity.getComponents())
+            classMap.put(component);
     }
 
     /**
-     * Retrieves all components in the container.
-     * @return
+     * Gets entities.
+     * @return entites
      */
-    public Collection<Component> getAllComponents() {
-        Collection<Component> components = new HashSet<>();
-        for (Entity entity : entities)
-            for (Component component : entity.getComponents())
-                components.add(component);
-        return components;
+    public Collection<Entity> getEntities() {
+        return entities;
+    }
+
+    /**
+     * Retrieves all components in the container of the type clazz.
+     * @param clazz the class type
+     * @return components of type class
+     */
+    public <T extends Component> Collection<T> getAllComponents(Class<T> clazz) {
+        return classMap.get(clazz);
     }
 }
