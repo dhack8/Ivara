2826e161b2584fc8c7560731b9c810df1ad8833f
David Hack <dhack8@gmail.com>
2017-09-16 18:48:49 +1200

    Added image loading and sprite drawing

--- src/main/java/backends/processing/PWindow.java
+++ src/main/java/backends/processing/PWindow.java
@@ -2,6 +2,7 @@ package backends.processing;
 
 import backends.InputBroadcaster;
 import backends.Renderer;
+import core.AssetHandler;
 import core.components.PSpriteComponent;
 import core.input.KeyListener;
 import core.input.MouseListener;
@@ -15,6 +16,7 @@ import processing.core.PApplet;
 public class PWindow extends PApplet implements InputBroadcaster, Renderer{
 
     private Scene currentScene;
+    private AssetHandler handler;
 
     @Override
     public void setMask(int mask) {
@@ -43,6 +45,24 @@ public class PWindow extends PApplet implements InputBroadcaster, Renderer{
         noLoop();
     }
 
+    /**
+     * Processing's setup, for now loads the images to the asset handler.
+     */
+    @Override
+    public void setup(){
+        AssetHandler.loadImage("./assets/dirt.png", "dirt");
+        AssetHandler.loadImage("./assets/dirt-bottom.png", "dirt-bottom");
+        AssetHandler.loadImage("./assets/grass-bottom.png", "grass-bottom");
+        AssetHandler.loadImage("./assets/grass-bottom-left.png", "grass-bottom-left");
+        AssetHandler.loadImage("./assets/grass-bottom-right.png", "grass-bottom-right");
+        AssetHandler.loadImage("./assets/grass-left.png", "grass-left");
+        AssetHandler.loadImage("./assets/grass-right.png", "grass-right");
+        AssetHandler.loadImage("./assets/grass-top.png", "grass-top");
+        AssetHandler.loadImage("./assets/grass-top-left.png", "grass-top-left");
+        AssetHandler.loadImage("./assets/grass-top-right.png", "grass-top-right");
+        AssetHandler.loadImage("./assets/player.png", "player");
+    }
+
     @Override
     public void draw(){
         for (Entity e : currentScene.getEntities()) {
@@ -54,6 +74,18 @@ public class PWindow extends PApplet implements InputBroadcaster, Renderer{
 
             rect(e.getPosition().x, e.getPosition().y, 10, 10);
         }
+    }
+
+    /**
+     * Draws a sprite in the given location.
+     * @param x x location
+     * @param y y location
+     * @param width width
+     * @param height height
+     * @param sprite the sprite to draw
+     */
+    private void drawSprite(int x, int y, int width, int height, PSpriteComponent sprite){
+        image(AssetHandler.getImage(sprite.getResourceID()), x, y, width, height);
     }
 
     @Override
