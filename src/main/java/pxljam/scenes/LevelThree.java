a03b8bf5f880fc14fd23472c5c6bd8afa065bf66
Alex <alexjfmitchell@gmail.com>
2017-09-17 16:38:40 +1200

    Small level 3 change

--- src/main/java/pxljam/scenes/LevelThree.java
+++ src/main/java/pxljam/scenes/LevelThree.java
@@ -2,6 +2,7 @@ package pxljam.scenes;
 
 import core.scene.Scene;
 import maths.Vector;
+import pxljam.entities.BackgroundEntity;
 import pxljam.entities.BasicBlockEntity;
 import pxljam.entities.MovingBlockEntity;
 import pxljam.entities.PlayerEntity;
@@ -12,6 +13,9 @@ import pxljam.entities.PlayerEntity;
 public class LevelThree extends Scene{
 
     public LevelThree(){
+        //addEntity(new BackgroundEntity(0f,0f));
+
+
         addEntity(new PlayerEntity(2, 1.5f));
 
         for(int i = 0; i < 4; i++){
@@ -24,7 +28,5 @@ public class LevelThree extends Scene{
         for(int i = 6; i < 11; i++){
             addEntity(new BasicBlockEntity(i,5, "grass-top"));
         }
-
-        //addEntity(new MovingBlockEntity(11, 5, "grass-top", 1f, 3f, 2));
     }
 }
