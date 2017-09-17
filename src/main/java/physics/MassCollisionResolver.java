5078f9befdc7252b6b84f1ccafac90ef1b8f47e9
Callum Li <CallumL1009@gmail.com>
2017-09-17 13:48:56 +1200

    Added static and dynamic bodies for physics

--- src/main/java/physics/MassCollisionResolver.java
+++ src/main/java/physics/MassCollisionResolver.java
@@ -21,15 +21,19 @@ public class MassCollisionResolver {
         ColliderComponent[] colliders = entities.getAllComponents(ColliderComponent.class).toArray(new ColliderComponent[0]);
 
         for (int i = 0; i < colliders.length; i++) {
+            ColliderComponent c1 = colliders[i];
+            PhysicsComponent pc1 = c1.getEntity().getComponents(PhysicsComponent.class).stream().findAny().orElse(new PhysicsComponent(c1.getEntity()));
+
+            if (pc1.getProperties().getType() == PhysicProperties.Type.STATIC) {
+                continue;
+            }
+
             for (int j = i+1; j < colliders.length; j++) {
-                ColliderComponent c1 = colliders[i];
                 ColliderComponent c2 = colliders[j];
 
                 if (CollisionUtil.intersect(c1.getCollider(), c2.getCollider())) {
 
-                    PhysicsComponent pc1 = c1.getEntity().getComponents(PhysicsComponent.class).stream().findAny().orElse(new PhysicsComponent(c1.getEntity()));
                     PhysicsComponent pc2 = c2.getEntity().getComponents(PhysicsComponent.class).stream().findAny().orElse(new PhysicsComponent(c2.getEntity()));
-
 
                     System.out.println(c1.getEntity() + " is colliding with " + c2.getEntity());
 
