86017c279f3d7e3cc1b2bb209840b28c80e6a5f7
Alex <alexjfmitchell@gmail.com>
2017-09-30 20:08:54 +1300

    Updated ScriptComponent to have multiple scripts

--- src/main/java/core/components/ScriptComponent.java
+++ src/main/java/core/components/ScriptComponent.java
@@ -4,17 +4,42 @@ import core.Script;
 import core.entity.GameEntity;
 import eem.Component;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
  * Script component belongs to an entity. Gets overridden with
  * proper implementation of a script. E.g. NPC1, NPC2, Player...
  */
 public class ScriptComponent extends Component<GameEntity> {
 
-    public final Script script;
+    //public final Script script;
 
+    public Set<Script> scripts;
+
+    /**
+     * Constructor for a ScriptComponent that takes a single script
+     * @param entity The entity for the script
+     * @param script The script to add
+     */
     public ScriptComponent(GameEntity entity, Script script) {
         super(entity);
-        this.script = script;
+        scripts = new HashSet<Script>();
+        //this.script = script;
+        scripts.add(script);
+    }
+
+    /**
+     * Constructor for a ScriptComponent that takes a collection of scripts
+     * @param entity The entity for the script
+     * @param scripts The scripts to add
+     */
+    public ScriptComponent(GameEntity entity, Collection<Script> scripts) {
+        super(entity);
+        this.scripts = new HashSet<Script>();
+        this.scripts.addAll(scripts);
     }
 
 }
