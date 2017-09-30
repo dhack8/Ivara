9d7952d5518ce516080a4f5e4d496f55f069f831
Callum Li <CallumL1009@gmail.com>
2017-09-30 23:13:18 +1300

    Fixed Platform

--- src/main/java/core/systems/GravitySystem.java
+++ src/main/java/core/systems/GravitySystem.java
@@ -1,11 +1,15 @@
 package core.systems;
 
+import core.components.PhysicsComponent;
 import core.components.VelocityComponent;
 import core.entity.GameEntity;
 import scew.System;
 import scew.World;
 import maths.Vector;
 import util.Debug;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Created by Callum Li on 9/30/17.
@@ -20,8 +24,13 @@ public class GravitySystem extends System<GameEntity> {
 
     @Override
     public void update(int dt, World<GameEntity> world) {
+
         Debug.log(Float.toString(gravity.y * (dt/1000f)));
-        world.get(VelocityComponent.class)
-                .forEach((c) -> c.add(gravity.x * (dt/1000f), gravity.y * (dt/1000f)));
+        Set<GameEntity> physicEntities = world.get(PhysicsComponent.class).stream()
+                .map((physicsComponent -> physicsComponent.getEntity()))
+                .collect(Collectors.toSet());
+        world.get(VelocityComponent.class).stream()
+                .filter(velocityComponent -> physicEntities.contains(velocityComponent.getEntity()))
+                .forEach((velocityComponent -> velocityComponent.getVelocity().add(gravity.x * (dt/1000f), gravity.y * (dt/1000f))));
     }
 }
