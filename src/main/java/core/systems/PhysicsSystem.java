dbc1a3a431022db7a7d8d101c00a4158131fccf9
Callum Li <CallumL1009@gmail.com>
2017-09-30 20:17:43 +1300

    Added Physics Improved

--- src/main/java/core/systems/PhysicsSystem.java
+++ src/main/java/core/systems/PhysicsSystem.java
@@ -24,31 +24,47 @@ public class PhysicsSystem extends System<GameEntity> {
 
 
         for (int i = 0; i < colliders.length; i++) {
-
-            for (int j = 0; j < colliders.length; j++) {
+            for (int j = i+1; j < colliders.length; j++) {
                 ColliderComponent cc1 = colliders[i];
                 ColliderComponent cc2 = colliders[j];
-                PhysicProperties p1 = cc1.getEntity().get(PhysicsComponent.class).getProperties();
-                PhysicProperties p2 = cc2.getEntity().get(PhysicsComponent.class).getProperties();
 
-                if (p1.type == PhysicProperties.Type.STATIC &&
-                        p2.type == PhysicProperties.Type.STATIC) {
-                    continue;
-                }
+                resolveCollision(cc1, cc2);
+            }
+        }
 
-                Collider  c1 = SceneUtils.colliderToWorld(cc1);
-                Collider c2 = SceneUtils.colliderToWorld(cc2);
+        for (int i = colliders.length - 1; i >= 0; i--) {
+            for (int j = i-1; j >= 0; j--) {
+                ColliderComponent cc1 = colliders[i];
+                ColliderComponent cc2 = colliders[j];
 
-                if (CollisionUtil.intersect(c1, c2)) {
-                    Vector v = CollisionUtil.minimumDistanceVector((AABBCollider) c1, (AABBCollider) c2);
+                resolveCollision(cc1, cc2);
+            }
+        }
+    }
 
-                    if (p1.inverseMass < p2.inverseMass) {
-                        cc2.getEntity().getTransform().add(v.x, v.y);
-                    } else {
-                        cc1.getEntity().getTransform().add(-v.x, -v.y);
-                    }
-                }
+    private void resolveCollision(ColliderComponent cc1, ColliderComponent cc2) {
+        PhysicProperties p1 = cc1.getEntity()
+                .get(PhysicsComponent.class).get().getProperties();
 
+        PhysicProperties p2 = cc2.getEntity()
+                .get(PhysicsComponent.class).get().getProperties();
+
+        if (p1.type == PhysicProperties.Type.STATIC &&
+                p2.type == PhysicProperties.Type.STATIC) {
+            return;
+        }
+
+        Collider c1 = SceneUtils.colliderToWorld(cc1);
+        Collider c2 = SceneUtils.colliderToWorld(cc2);
+
+        if (CollisionUtil.intersect(c1, c2)) {
+            Vector v = CollisionUtil.minimumDistanceVector((AABBCollider) c1, (AABBCollider) c2);
+
+            if (p2.getType() != PhysicProperties.Type.STATIC ) {
+                cc2.getEntity().getTransform().add(v.x, v.y);
+            }
+            if (p1.getType() != PhysicProperties.Type.STATIC) {
+                cc1.getEntity().getTransform().add(-v.x, -v.y);
             }
         }
     }
