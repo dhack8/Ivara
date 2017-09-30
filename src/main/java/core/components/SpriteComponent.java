4de1f22e5483d4ecc0b9dd1a4d408f847d7d9125
David Hack <dhack8@gmail.com>
2017-10-01 11:20:07 +1300

    Added java docs

--- src/main/java/core/components/SpriteComponent.java
+++ src/main/java/core/components/SpriteComponent.java
@@ -11,12 +11,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by Callum Li on 9/15/17.
+ * Sprite component holds sprites and is the only sprite component to the parent entity
+ * @author David Hack
  */
 public class SpriteComponent extends Component<GameEntity> {
 
-
+    //Basic non animated sprites
     private final List<Sprite> sprites = new ArrayList<>();
+
+    //Animated sprites
     private final List<AnimatedSprite> animatedSprites = new ArrayList<>();
 
     /**
@@ -36,18 +39,37 @@ public class SpriteComponent extends Component<GameEntity> {
         sprites.add(s);
     }
 
+    /**
+     * Adds a new sprite from required parameters
+     * @param id resourceID object
+     * @param transform transform vector
+     * @param dimension dimensions vector
+     */
     public void add(ResourceID id, Vector transform, Vector dimension){
         sprites.add(new Sprite(id, transform, dimension));
     }
 
+    /**
+     * Adds a new sprite with no transform from the parent entity
+     * @param id resourceID object
+     * @param dimensions dimensions vector
+     */
     public void add(ResourceID id, Vector dimensions){
         sprites.add(new Sprite(id, new Vector(0,0), dimensions));
     }
 
+    /**
+     * Getter for the basic sprites
+     * @return list of basic sprites
+     */
     public List<Sprite> getSprites(){
         return sprites;
     }
 
+    /**
+     * Getter for the animatedSprites
+     * @return list of animated sprites
+     */
     public List<AnimatedSprite> getAnimatedSprites() {
         return animatedSprites;
     }
