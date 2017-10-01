51d09e40296887c7a23c8cfed3ba0a9ecbea847d
David Hack <dhack8@gmail.com>
2017-10-01 13:17:56 +1300

    Fixed player entity

--- src/main/java/ivara/entities/PlayerEntity.java
+++ src/main/java/ivara/entities/PlayerEntity.java
@@ -21,12 +21,13 @@ import java.util.Set;
  * This class handles the entity of the player within the game
  *
  * @author Alex Mitchell
+ * @author David Hack
  */
 public class PlayerEntity extends GameEntity {
 
-    //Vector previous = new Vector(0,0);
-
+    //Restricts jumping
     public boolean canJump = false;
+
     /**
      * Creates a PlayerEntity at a given position
      *
@@ -36,40 +37,32 @@ public class PlayerEntity extends GameEntity {
     public PlayerEntity(float x, float y) {
         super(new Vector(x, y));
 
-        //Velocity
+        //Velocity---
         VelocityComponent v = new VelocityComponent(this);
         addComponent(v);
 
-        //Sprites
+        //Sprites---
         SpriteComponent sc = new SpriteComponent(this);
         sc.add(new ResourceID("player"), new Vector(1f, 1.5f));
         addComponent(sc);
 
-        //Scripts
-        Set<Script> scripts = new HashSet<>(); // added multiple scripts
+        //Scripts---
         PlayerController pc = new PlayerController(this);
-        scripts.add(pc);
+        addComponent(new ScriptComponent(this, pc));
 
-        addComponent(new ScriptComponent(this, scripts));
-        //addComponent(new Gravity(this));
-        //addComponent(new Gravity());
-
+        //Collider---
         addComponent(new ColliderComponent(this, new AABBCollider(AABBCollider.TOPLEFT, new Vector(0.2f, 0.3f), new Vector(0.6f, 1.2f)))); //Todo Change the Collider component
+
+        //Layer---
         addComponent(new LayerComponent(this, 999));
-        //addComponent(new PhysicsComponent(this, 1, PhysicProperties.Type.DYNAMIC));
+
+        //Physics---
         addComponent(new PhysicsComponent(this, new PhysicProperties(1, PhysicProperties.Type.DYNAMIC)));
 
-        addComponent(
-                new SensorComponent(
-                        this,
-                        new AABBCollider(
-                                AABBCollider.TOPLEFT,
-                                new Vector(0.199f, 1.4f),
-                                new Vector(0.611f, 0.1f)
-                        ),pc));
-        //addComponent(new CameraComponent(this, 19));
-        //addComponent(new CameraComponent(this, new Camera()));
-
+        //Sensors---
+        //AABB for the sensor
+        AABBCollider ab = new AABBCollider(AABBCollider.TOPLEFT, new Vector(0.199f, 1.4f), new Vector(0.611f, 0.1f));
+        addComponent(new SensorComponent(this, new Sensor(ab, pc)));
     }
 }
 
