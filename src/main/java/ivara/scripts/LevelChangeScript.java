f94e6397f5560122934d10b71f6b3c3c3c3e2107
Alex <alexjfmitchell@gmail.com>
2017-10-03 20:39:23 +1300

    Added changes to changelevelscript

--- src/main/java/ivara/scripts/LevelChangeScript.java
+++ src/main/java/ivara/scripts/LevelChangeScript.java
@@ -2,11 +2,23 @@ package ivara.scripts;
 
 import core.Script;
 import core.entity.GameEntity;
+import core.struct.Sensor;
 
 /**
  * Created by Alex Mitchell on 3/10/2017.
  */
 public class LevelChangeScript implements Script{
+    private final GameEntity entity;
+    private final Sensor sensor;
+
+    /**
+     * Create a LevelChangeScript
+     */
+    public LevelChangeScript(GameEntity e, Sensor s){
+        this.entity = e;
+        this.sensor = s;
+    }
+
     @Override
     public void update(int dt, GameEntity entity) {
 
