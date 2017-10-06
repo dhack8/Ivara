5b3b877e475f8786f33119c5daebeb8510b91e8d
David Hack <dhack8@gmail.com>
2017-10-06 20:30:29 +1300

    Increaced sleep timmer

--- src/main/java/backends/processing/PWindow.java
+++ src/main/java/backends/processing/PWindow.java
@@ -74,7 +74,7 @@ public class PWindow extends PApplet implements InputBroadcaster, Renderer{
 
         while(drawing){
             try {
-                Thread.sleep(0, 2);
+                Thread.sleep(0, 5);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
