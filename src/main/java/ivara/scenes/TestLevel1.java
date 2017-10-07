a6264de75c505f6512a643c6ac6520ad86c9449b
Alex <alexjfmitchell@gmail.com>
2017-10-07 17:58:51 +1300

    Added basic functionality for a clickable entity + script

--- src/main/java/ivara/scenes/TestLevel1.java
+++ src/main/java/ivara/scenes/TestLevel1.java
@@ -2,10 +2,7 @@ package ivara.scenes;
 
 import core.scene.Scene;
 import core.struct.Camera;
-import ivara.entities.BasicBlockEntity;
-import ivara.entities.NPlatformEntity;
-import ivara.entities.PlayerEntity;
-import ivara.entities.NewBasicEnemyEntity;
+import ivara.entities.*;
 import maths.Vector;
 
 /**
@@ -16,7 +13,8 @@ public class TestLevel1 extends Scene{
         addEntity(new BasicBlockEntity(0,1,"dirt"));
         addEntity(new NPlatformEntity(1,2,3, false));
         addEntity(new BasicBlockEntity(4,2.1f,"dirt"));
-        addEntity(new PlayerEntity(0,0));
+        //addEntity(new PlayerEntity(0,0));
+        addEntity(new StartGameEntity(0,0));
         addEntity(new NewBasicEnemyEntity(new Vector(2,1), "slime"));
 
         addEntity(new NPlatformEntity(5f, 1f, 4, true, 7f, 3f, 3f));
