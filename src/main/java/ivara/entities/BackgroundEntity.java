b621161cd1a02cc3aa014e3e91fad00a4dd0275b
Alex <alexjfmitchell@gmail.com>
2017-10-11 12:48:10 +1300

    Tweaks to background entity

--- src/main/java/ivara/entities/BackgroundEntity.java
+++ src/main/java/ivara/entities/BackgroundEntity.java
@@ -33,9 +33,10 @@ public class BackgroundEntity extends GameEntity {
     }
 
     /**
-     * Creates a background entity at the specified location, on the -1 layer (behind all entities in the foreground).
+     * Creates a background entity which also has a death line.
      * @param x x location
      * @param y y location
+     * @param deathPosStart Starting position for a death-line that resets the level on ANY collision
      */
     public BackgroundEntity(float x, float y, Vector deathPosStart){
         super(new Vector(x,y));
