2406670a1c4566258ec12cfc7c71b4649297368f
Callum Li <CallumL1009@gmail.com>
2017-10-12 11:31:51 +1300

    Coin Entity Iteration 3

---
+++ src/main/java/ivara/entities/CoinTextEntity.java
@@ -0,0 +1,33 @@
+package ivara.entities;
+
+import core.Script;
+import core.components.ScriptComponent;
+import core.components.TextComponent;
+import core.entity.GameEntity;
+import core.struct.Text;
+import maths.Vector;
+
+/**
+ * Created by Callum Li on 10/12/17.
+ */
+public class CoinTextEntity extends GameEntity {
+
+    private final TextComponent score;
+
+    public CoinTextEntity(Vector transform, PlayerEntity player) {
+        super(transform);
+
+        score = new TextComponent(this, new Text(16, "0"));
+
+        addComponent(score);
+
+        addComponent(new ScriptComponent(
+                this,
+                new Script() {
+                    @Override
+                    public void update(int dt, GameEntity entity) {
+                        Integer.toString(player.coinsCollected);
+                    }
+                }));
+    }
+}
