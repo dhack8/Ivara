be775bcf8ebf5b5f32b6cbe1f8b01292af850078
David Hack <dhack8@gmail.com>
2017-10-12 19:03:34 +1300

    Added fields

--- src/main/java/ivara/scenes/DefaultScene.java
+++ src/main/java/ivara/scenes/DefaultScene.java
@@ -10,10 +10,14 @@ import maths.Vector;
  * Created by David Hack Local on 12-Oct-17.
  */
 public class DefaultScene extends Scene {
+
+    private static final Vector timerLoc = new Vector(1.5f,1.5f);
+    private static final Vector coinLoc = new Vector(2.1f, 2.3f);
+
     public void startScene(){}
 
     public void startScene(PlayerEntity player){
-        addEntity(new TimerEntity(new Vector(1.5f,1.5f), 0));
-        addEntity(new CoinTextEntity(new Vector(2.1f, 2.3f), player));
+        addEntity(new TimerEntity(timerLoc, 0));
+        addEntity(new CoinTextEntity(coinLoc, player));
     }
 }
