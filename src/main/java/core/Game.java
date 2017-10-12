5fab70be857689bfed261055eefa53e7fc942871
David Hack <dhack8@gmail.com>
2017-10-12 18:11:07 +1300

    mask on

--- src/main/java/core/Game.java
+++ src/main/java/core/Game.java
@@ -38,7 +38,7 @@ public abstract class Game {
         this.inputHandler = new InputHandler(inputBroadcaster);
         this.levelManager = lm;
 
-        renderer.setMask(1);
+        //renderer.setMask(1);
 
         lm.setGame(this);
 
