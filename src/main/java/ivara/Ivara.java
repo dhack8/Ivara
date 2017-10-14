198d694476b7ad5b44fb1818d0c7f3e16ae9c0a3
David Hack <dhack8@gmail.com>
2017-10-14 17:35:16 +1300

    Added level 2

--- src/main/java/ivara/Ivara.java
+++ src/main/java/ivara/Ivara.java
@@ -48,7 +48,7 @@ public class Ivara extends Game {
         List<Scene> levels = new ArrayList<>();
 
         levels.add(new StartMenu());
-        levels.add(new Level1());
+        levels.add(new Level2());
         levels.add(new TestLevel1());
         levels.add(new TestLevel2());
         return levels;
