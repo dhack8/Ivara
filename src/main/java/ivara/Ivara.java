e848d606c6ecf6beb958e8cb3f9879f3b6fc0072
Will Pearson <will.pearson@xtra.co.nz>
2017-10-14 23:05:23 +1300

    Added super level

--- src/main/java/ivara/Ivara.java
+++ src/main/java/ivara/Ivara.java
@@ -48,7 +48,7 @@ public class Ivara extends Game {
         List<Scene> levels = new ArrayList<>();
 
         levels.add(new StartMenu());
-        levels.add(new HardLevel2());
+        levels.add(new Mountain_fun_time_of_doom());
         return levels;
     }
 
