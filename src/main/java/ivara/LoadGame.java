83e5479fc9f8e8b244687378c58817b2ed79f649
Alex <alexjfmitchell@gmail.com>
2017-10-14 18:58:07 +1300

    Added some more to loadGame

--- src/main/java/ivara/LoadGame.java
+++ src/main/java/ivara/LoadGame.java
@@ -1,8 +1,19 @@
 package ivara;
 
+import core.entity.GameEntity;
+import core.scene.LevelManager;
+import core.scene.Scene;
+import ivara.entities.PlayerEntity;
+import ivara.scenes.DefaultScene;
+import maths.Vector;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 /**
  * Created by Alex Mitchell on 14/10/2017.
@@ -10,11 +21,34 @@ import java.io.FileReader;
 public class LoadGame {
     public static final String fileName = "./savefile.sav";
 
-    public static void load(){
+    public static boolean load(LevelManager lm){
         File file = new File(fileName);
-        FileReader fr = new FileReader(file);
-        BufferedReader br = new BufferedReader(fr);
 
-        br.readLine()
+        try{
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+            Iterator<String> i = br.lines().iterator();
+            int levelNum = Integer.parseInt(i.next());
+            Vector spawnPos = new Vector(Float.parseFloat(i.next()), Float.parseFloat(i.next()));
+            float time = Float.parseFloat(i.next());
+            Set<Vector> coins = new HashSet<>();
+            while(i.hasNext()){
+                coins.add(new Vector(Float.parseFloat(i.next()), Float.parseFloat(i.next())));
+            }
+
+
+            DefaultScene startScene = (DefaultScene) lm.getScene(levelNum);
+            startScene.setSpawn(spawnPos);
+
+            GameEntity p = startScene.getEntity(PlayerEntity.class);
+           // if(){
+
+            //}
+
+        }catch(IOException e){
+            System.err.println(e);
+            return false;
+        }
+        return true;
     }
 }
