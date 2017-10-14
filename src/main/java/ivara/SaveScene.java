7c63b5f648cb9eb53bfadc53737d5bae0a4002c8
James Magallanes <jamesmagallanes@gmail.com>
2017-10-14 17:21:57 +1300

    SaveScene template

---
+++ src/main/java/ivara/SaveScene.java
@@ -0,0 +1,22 @@
+package ivara;
+
+import core.Game;
+import core.scene.LevelManager;
+import core.scene.Scene;
+
+import java.io.File;
+
+/**
+ * Created by james on 12/10/2017.
+ */
+public class SaveScene {
+
+    private Game game;
+    private LevelManager levelManager;
+    private Scene scene;
+    private File file = new File("./savefile.txt");
+
+    public static void save(Game game){
+        
+    }
+}
