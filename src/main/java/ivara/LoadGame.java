991a1ea12c2da898262dd9121df6005e8aa6f851
Alex <alexjfmitchell@gmail.com>
2017-10-14 20:02:46 +1300

    Added more content to the loadgame class

--- src/main/java/ivara/LoadGame.java
+++ src/main/java/ivara/LoadGame.java
@@ -1,9 +1,14 @@
 package ivara;
 
+import core.components.TextComponent;
 import core.entity.GameEntity;
 import core.scene.LevelManager;
 import core.scene.Scene;
+import core.struct.Text;
+import core.struct.Timer;
+import ivara.entities.CoinEntity;
 import ivara.entities.PlayerEntity;
+import ivara.entities.TimerEntity;
 import ivara.scenes.DefaultScene;
 import maths.Vector;
 
@@ -11,9 +16,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created by Alex Mitchell on 14/10/2017.
@@ -28,9 +31,10 @@ public class LoadGame {
             FileReader fr = new FileReader(file);
             BufferedReader br = new BufferedReader(fr);
             Iterator<String> i = br.lines().iterator();
+
             int levelNum = Integer.parseInt(i.next());
             Vector spawnPos = new Vector(Float.parseFloat(i.next()), Float.parseFloat(i.next()));
-            float time = Float.parseFloat(i.next());
+            String time = i.next();
             Set<Vector> coins = new HashSet<>();
             while(i.hasNext()){
                 coins.add(new Vector(Float.parseFloat(i.next()), Float.parseFloat(i.next())));
@@ -40,16 +44,30 @@ public class LoadGame {
             DefaultScene startScene = (DefaultScene) lm.getScene(levelNum);
             startScene.setSpawn(spawnPos);
 
-            //Set
+            //Set the timer
+            GameEntity t = startScene.getEntity(TimerEntity.class);
+            if(t != null){
+                TimerEntity timer = (TimerEntity) t;
+                TextComponent textComponent = timer.get(TextComponent.class).get(); // timer entity should have a text component
+                Optional<Text> opText = textComponent.getTexts().stream().findFirst();
+                Text text;
+                if(opText.isPresent())text = opText.get();
+                else throw new RuntimeException("Could not find text in the timer when saving.");
+                textComponent.clear(); // clear the current texts
+                textComponent.add(text.transform, time, text.fontSize);
+            } else throw new RuntimeException("Could not find the textComponent.");
 
             //Set coins of the player
             GameEntity p = startScene.getEntity(PlayerEntity.class);
             if(p != null){
                 PlayerEntity player = (PlayerEntity)p;
                 player.coinsCollected = coins.size();
-            }
+            }else throw new RuntimeException("Could not find a player entity.");
 
-        }catch(IOException e){
+            //Remove the coins from the scene
+            //startScene.getEntities(CoinEntity.class).stream().filter((e) -> e.) // todo still on here
+
+        }catch(Exception e){
             System.err.println(e);
             return false;
         }
