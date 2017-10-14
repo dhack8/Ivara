862c8c8da1f2324170f2781929c5b17b4bea00e4
Alex <alexjfmitchell@gmail.com>
2017-10-14 18:29:48 +1300

    Added small stuff to default scene - wont compile

--- src/main/java/ivara/LoadGame.java
+++ src/main/java/ivara/LoadGame.java
@@ -1,5 +1,9 @@
 package ivara;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
 /**
  * Created by Alex Mitchell on 14/10/2017.
  */
@@ -7,6 +11,10 @@ public class LoadGame {
     public static final String fileName = "./savefile.sav";
 
     public static void load(){
+        File file = new File(fileName);
+        FileReader fr = new FileReader(file);
+        BufferedReader br = new BufferedReader(fr);
 
+        br.readLine()
     }
 }
