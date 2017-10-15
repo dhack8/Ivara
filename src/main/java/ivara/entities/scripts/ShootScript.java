2fd9b8a2afd275e527f9f2ef3dd5947edc7fcea0
Alex <alexjfmitchell@gmail.com>
2017-10-15 22:03:43 +1300

    Commented and tidied up ShootScript

--- src/main/java/ivara/entities/scripts/ShootScript.java
+++ src/main/java/ivara/entities/scripts/ShootScript.java
@@ -1,61 +1,73 @@
 package ivara.entities.scripts;
 
+
 import core.Script;
 import core.entity.GameEntity;
 import core.struct.ResourceID;
 import core.struct.Timer;
 import ivara.entities.BulletEntity;
-import ivara.entities.PlayerEntity;
 import maths.Vector;
 
 import java.util.Arrays;
 
 /**
- * A shooting script that shoots at a target.
- * This script uses the updated bullet entity that utilizes velocity rather than time.
- * If a projectile collides with an instance of the target, the game resets
+ * A shooting script that shoots a BulletEntity at a target.
+ * The projectile moves at a constant velocity toward the target.
+ * @author Alex Mitchell
+ * @author Callum Li
  */
 public class ShootScript implements Script {
+    private static final int DELAY = 2000; // Delay after a shot
+    private static final int DURATION = 2000; // Delay until a bullet is reset
+    private static final float SPEED = 3f; // In meters per second (has x and y components that in total equal 3f)
 
     private GameEntity entity;
     private GameEntity target;
+    private Timer t; // Delay timer
+    private Vector offset; // Vector offsetting aim
 
-    private Timer t; // delay timer
-    private final int DELAY = 2000; // delay after shot
-    private final int DURATION = 2000; // how long till the bullet resets
-    private final float SPEED = 3f; // in ms^-1
-    private Vector offset; //vector offsetting aim
-
-
+    /**
+     * Constructs a shooting script that fires projectiles at a specified target entity.
+     * This constructor takes a specified offset to offset the projectile firing location.
+     * @param entity The entity shooting.
+     * @param target The target.
+     * @param offset The vector representing x and y offset.
+     */
     public ShootScript(GameEntity entity, GameEntity target, Vector offset){
         this.entity = entity;
         this.target = target;
         this.offset = offset;
-        t = new Timer(0, ()->{});
+        t = new Timer(0, ()->{}); // Initialise timer
     }
 
+    /**
+     * Constructs a shooting script that fires projectiles at a specified target entity.
+     * @param entity The entity shooting.
+     * @param target The target.
+     */
     public ShootScript(GameEntity entity, GameEntity target){
         this.entity = entity;
         this.target = target;
         this.offset = new Vector(0,0);
-        t = new Timer(0, ()->{});
+        t = new Timer(0, ()->{}); // Initialise timer
     }
 
     @Override
     public void update(int dt, GameEntity entity) {
-        if(t.isFinished()){
+        if(t.isFinished()){ // Only fire a bullet after the delay
             GameEntity bullet = new BulletEntity(
                     entity.transform,
                     new Vector(target.getTransform().x + offset.x, target.getTransform().y + offset.y),
                     SPEED,new ResourceID("slimeball"),
                     target.getClass(),
-                    //Arrays.asList(entity.getClass(),target.getClass())
                     Arrays.asList(entity.getClass())
             );
 
+            // Add bullet to the scene
             entity.getScene().addEntity(bullet);
             entity.getScene().addTimer(new Timer(DURATION, ()->entity.getScene().removeEntity(bullet)));
-            //reset delay timer
+
+            // Reset delay timer
             t = new Timer(
                     DELAY,
                     ()->{}
@@ -64,6 +76,4 @@ public class ShootScript implements Script {
         }
 
     }
-
-
 }
