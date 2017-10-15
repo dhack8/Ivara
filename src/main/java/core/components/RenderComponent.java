3b440042714c8c963f3414b1734ad2ac6885a4ab
David Hack <dhack8@gmail.com>
2017-10-15 23:42:02 +1300

    All components fully JDoced

--- src/main/java/core/components/RenderComponent.java
+++ src/main/java/core/components/RenderComponent.java
@@ -5,11 +5,13 @@ import scew.Component;
 
 /**
  * Layer component gives a entity a layer for rendering.
+ * And other render types.
  *
  * @author David Hack
  */
 public class RenderComponent extends Component<GameEntity> {
 
+    //Types
     public enum Mode{
         NORMAL,
         PIXEL_NO_TRANS,
@@ -20,8 +22,8 @@ public class RenderComponent extends Component<GameEntity> {
     private Mode mode = Mode.NORMAL;
 
     /**
-     * Creates a layer component with an int value
-     * @param entity parent entity
+     * Creates a layer component with an int value.
+     * @param entity parent of component
      * @param layer layer of entity
      */
     public RenderComponent(GameEntity entity, int layer) {
@@ -29,33 +31,64 @@ public class RenderComponent extends Component<GameEntity> {
         this.layer = layer;
     }
 
+    /**
+     * Crates a render component with just a mode.
+     * @param entity parent of component
+     * @param mode mode of rendering
+     */
     public RenderComponent(GameEntity entity, Mode mode) {
         super(entity);
         this.mode = mode;
     }
 
+    /**
+     * Creates a rendering component with all the parameters
+     * @param entity parent of component
+     * @param layer layer of entity
+     * @param mode mode of rendering
+     */
     public RenderComponent(GameEntity entity, int layer, Mode mode) {
         super(entity);
         this.layer = layer;
         this.mode = mode;
     }
 
+    /**
+     * Creates a rendering component with no other parameters.
+     * @param entity parent of component
+     */
     public RenderComponent(GameEntity entity) {
         super(entity);
     }
 
+    /**
+     * Getter for the layer.
+     * @return layer value
+     */
     public int getLayer() {
         return layer;
     }
 
+    /**
+     * Setter for the layer.
+     * @param layer layer value
+     */
     public void setLayer(int layer) {
         this.layer = layer;
     }
 
+    /**
+     * Returns the render mode.
+     * @return render mode
+     */
     public Mode getMode() {
         return mode;
     }
 
+    /**
+     * Setter for the mode
+     * @param mode render mode
+     */
     public void setMode(Mode mode) {
         this.mode = mode;
     }
--- src/main/java/core/components/VelocityComponent.java
+++ src/main/java/core/components/VelocityComponent.java
@@ -7,21 +7,30 @@ import maths.Vector;
 /**
  * Holds the velocity of an GameEntity.
  *
- * @author Will Pearson
+ * @author Will Pearson & Others
  */
 public class VelocityComponent extends Component<GameEntity> {
 
     private final Vector velocity;
     private boolean paused = false;
 
+    /**
+     * Constructs a new velocity component with a 0,0 velocity.
+     * @param entity parent of the component
+     */
     public VelocityComponent(GameEntity entity) {
         super(entity);
         this.velocity = new Vector(0, 0);
     }
 
+    /**
+     * Constructs a new velocity component with a given velocity.
+     * @param entity parent of the component
+     * @param velocity the desired initial velocity
+     */
     public VelocityComponent(GameEntity entity, Vector velocity) {
         super(entity);
-        this.velocity = new Vector(velocity.x, velocity.y);
+        this.velocity = new Vector(velocity);
     }
 
     /**
@@ -37,31 +46,52 @@ public class VelocityComponent extends Component<GameEntity> {
      * Gets velocity.
      * @return velocity
      */
-    public Vector getVelocity() { // todo working on this
+    public Vector getVelocity() {
         return  paused? new Vector(0,0) : velocity;
-//        return velocity;
     }
 
+    /**
+     * Setter for the velocity.
+     * @param velocity velocity
+     */
     public void set(Vector velocity) {
         this.velocity.setAs(velocity);
     }
 
+    /**
+     * Setter for the X part of the velocity vector
+     * @param x x value
+     */
     public void setX(float x) { // Todo change how this is done
         velocity.setAs(x,velocity.y);
     }
 
+    /**
+     * Setter for the Y part of the velocity vector
+     * @param y y value
+     */
     public void setY(float y) { // Todo change how this is done :)
         velocity.setAs(velocity.x,y);
     }
 
+    /**
+     * Pauses the velocity is not updated by velocity system.
+     */
     public void pause(){
         paused = true;
     }
 
+    /**
+     * Unpauses the velocity it is no updated by the velocity system.
+     */
     public void unpause(){
         paused = false;
     }
 
+    /**
+     * Returns the boolean defining pauses status.
+     * @return the pause boolean
+     */
     public boolean isPaused(){
         return paused;
     }
