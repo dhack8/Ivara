5f0b8fa39ff2ad205722a30db4e21e5573901e1c
Alex <alexjfmitchell@gmail.com>
2017-10-15 17:04:41 +1300

    Tidied up the LoadGame code

--- src/main/java/ivara/LoadGame.java
+++ src/main/java/ivara/LoadGame.java
@@ -15,6 +15,7 @@ import ivara.entities.TimerEntity;
 import ivara.entities.scripts.TimerScript;
 import ivara.scenes.DefaultScene;
 import maths.Vector;
+import util.Debug;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -31,74 +32,76 @@ public class LoadGame {
     public static final String fileName = "./savefile.sav";
 
     public static boolean load(Game game){
-        LevelManager lm = game.getLevelManager();
         File file = new File(fileName);
         try{
             FileReader fr = new FileReader(file);
             BufferedReader br = new BufferedReader(fr);
-            List<String> strings = Arrays.asList(br.readLine().split(" "));
+            List<String> data = Arrays.asList(br.readLine().split(" ")); // The information in the file only exists on a single line
 
-            Iterator<String> i = strings.iterator();
-            int levelNum = Integer.parseInt(i.next());
-            Vector spawnPos = new Vector(Float.parseFloat(i.next()), Float.parseFloat(i.next()));
-            long start = Long.parseLong(i.next());
-            long currentTime = Long.parseLong(i.next());
+            // Get the data as an iterator
+            Iterator<String> dataIterator = data.iterator();
+            // Number of the level to open up
+            int levelNum = Integer.parseInt(dataIterator.next());
+            // Checkpoint position that the player should spawn at
+            Vector spawnPos = new Vector(Float.parseFloat(dataIterator.next()), Float.parseFloat(dataIterator.next()));
+            // Timer information
+            long start = Long.parseLong(dataIterator.next());
+            long currentTime = Long.parseLong(dataIterator.next());
+            // Coins to remove
             List<Vector> coins = new ArrayList<Vector>();
-            while(i.hasNext()){
-                coins.add(new Vector(Float.parseFloat(i.next()), Float.parseFloat(i.next())));
+            while(dataIterator.hasNext()){ // Assuming that if there is one item next in the file, that there is another following it (i.e. 2 items)
+                coins.add(new Vector(Float.parseFloat(dataIterator.next()), Float.parseFloat(dataIterator.next())));
             }
 
+            // Change to the new scene
+            game.setCurrentScene(levelNum);
 
-            lm.setScene(levelNum);//Todo moved
-            //Set spawn point
-            DefaultScene startScene = (DefaultScene) lm.getScene(levelNum);
-            System.out.println("Level num: " + levelNum);
-            startScene.setSpawn(spawnPos);
+            // Get the targeted scene
+            Scene scene = game.getScene(levelNum);
+            if(!(scene instanceof DefaultScene)){Debug.log("Scene is not a DefaultScene, of which it needs to be.");return false;}
+            DefaultScene loadedLevel = (DefaultScene)scene;
+
+            //Set the spawn point
+            loadedLevel.setSpawn(spawnPos);
 
             //Set the timer
-            GameEntity t = startScene.getEntity(TimerEntity.class);
-            if(t != null){
+            GameEntity t = loadedLevel.getEntity(TimerEntity.class);
+            if(t != null){ // Check that there is a timer entity
                 TimerEntity timer = (TimerEntity) t;
-                ScriptComponent textComponent = timer.get(ScriptComponent.class).get(); // timer entity should have a script component
-                TimerScript ts = (TimerScript)textComponent.getScripts().stream().findFirst().get();// Assume the timer script exists
+                Optional<ScriptComponent> opScriptComp = timer.get(ScriptComponent.class);
+                ScriptComponent scriptComp;
+                if(opScriptComp.isPresent()){ // Check that there is a script component
+                    scriptComp = opScriptComp.get();
+                    Optional<Script> opScript = scriptComp.getScripts().stream().filter((e) -> e.getClass() == TimerScript.class).findFirst();
+                    TimerScript timerScript;
+                    if(opScript.isPresent())timerScript = (TimerScript) opScript.get(); // Check that there is a timer script
+                    else{Debug.log("Timer should have had a TimerScript but didn't.");return false;}
+                    timerScript.currentTime = currentTime;
+                    timerScript.start = start;
+                }else{Debug.log("Timer should have a script component but doesn't.");return false;}
 
-                ts.currentTime = currentTime;
-                ts.start = start;
-
-            } else throw new RuntimeException("Could not find the textComponent.");
+            } else {Debug.log("There should be a Timer in the scene, but there wasn't.");return false;}
 
             //Set coins of the player
-            GameEntity p = startScene.getEntity(PlayerEntity.class);
+            GameEntity p = loadedLevel.getEntity(PlayerEntity.class);
             if(p != null){
                 PlayerEntity player = (PlayerEntity)p;
                 player.coinsCollected = coins.size();
                 player.getTransform().setAs(spawnPos);
-            }else throw new RuntimeException("Could not find a player entity.");
+            }else {Debug.log("There should be a player in the scene, but there wasn't.");return false;}
 
-            //Remove the coins from the scene
-            System.out.println(coins.size());
-            System.out.println("To delete");
-            for(Vector v : coins){
-                System.out.println(v);
-            }
-
-            System.out.println("All coins");
-            Collection<GameEntity> removeableCoins = startScene.getEntities(CoinEntity.class).stream().filter((e) ->coins.contains(e.getTransform())).collect(Collectors.toSet());
-            for(GameEntity entity : startScene.getEntities(CoinEntity.class)){
-                System.out.println(entity.getTransform());
-            }
-            System.out.println("Need to remove: " + removeableCoins.size());
-            startScene.bankCoins(removeableCoins);
-            removeableCoins.stream().forEach((e)->startScene.removeEntityRegardless(e)); // remove the coin entities from the scene
-
+            //Remove the already collected coins from the scene
+            Collection<GameEntity> removeableCoins = loadedLevel.getEntities(CoinEntity.class).stream().filter((e) ->coins.contains(e.getTransform())).collect(Collectors.toSet()); // Get coins that have the same position as the ones that we are looking to delete from the scene
+            loadedLevel.bankCoins(removeableCoins); // Update the already collected coins in the scene
+            for(GameEntity e : removeableCoins)loadedLevel.removeEntityRegardless(e); // Remove the already collected coin entities from the scene
             fr.close();
 
-
-        }catch(Exception e){
-            System.err.println(e);
+        }catch(IOException e){
+            Debug.log(e.toString());
             return false;
         }
 
+        Debug.log("Successfully loaded a the file.");
         return true;
     }
 }
