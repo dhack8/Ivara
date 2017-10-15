099c7f15d76a28d9a2149ca5cf484eba73c1ced9
David Hack <dhack8@gmail.com>
2017-10-15 23:10:57 +1300

    Added renderer JDocs

--- src/main/java/backends/Renderer.java
+++ src/main/java/backends/Renderer.java
@@ -6,13 +6,22 @@ import processing.core.PApplet;
 import java.util.concurrent.CountDownLatch;
 
 /**
- * Created by Callum Li on 9/16/17.
+ * Generalization of the renderer that grantees mask setting and render call with a scene.
  */
 public abstract class Renderer extends PApplet{
-    int SPRITE = 1;
-    int COLLIDIER = 2;
+    public static final int SPRITE = 1;
+    public static final int COLLIDIER = 2;
 
+    /**
+     * A mask method for debugging.
+     * @param mask type of mask
+     */
     public abstract void setMask(int mask);
 
+    /**
+     * A render method that takes a scene and draws it.
+     * This process should be 'blocking'
+     * @param scene the scene to draw.
+     */
     public abstract void render(Scene scene);
 }
