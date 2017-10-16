f05798295206f75d9756a1133593ce1d7180590f
Alex <alexjfmitchell@gmail.com>
2017-10-16 13:01:24 +1300

    Documented the two BasicEnemyEntities

--- src/main/java/ivara/entities/enemies/SlimeEntity.java
+++ src/main/java/ivara/entities/enemies/SlimeEntity.java
@@ -8,13 +8,19 @@ import maths.Vector;
 
 /**
  * Slime entity that patrols back and forth between two points. The slime is able to be killed by the player if player
- * jumps on the slime's head. Damages player if it comes in contact with.
+ * jumps on the slime's head. Kills player the player if it comes into contact with the player.
+ * @author Alex Mitchell
+ * @author David Hack
  */
 public class SlimeEntity extends BasicEnemyEntity implements Enemy {
-
+    // Constants
     private final static float WIDTH = 1f;
     private final static float HEIGHT = 1f;
 
+    /**
+     * Constructs a slime entity that moves until it detects a collision or no block underneath it.
+     * @param location The initial position.
+     */
     public SlimeEntity(Vector location){
         super(location, new Vector(WIDTH, HEIGHT));
         Sprite as = new Sprite(new ResourceID("slime"), new Vector(0,0), new Vector(WIDTH, HEIGHT));
--- src/main/java/ivara/entities/enemies/SnakeEntity.java
+++ src/main/java/ivara/entities/enemies/SnakeEntity.java
@@ -1,38 +1,45 @@
 package ivara.entities.enemies;
 
 import core.struct.AnimatedSprite;
-import ivara.entities.enemies.BasicEnemyEntity;
-import ivara.entities.enemies.Enemy;
 import maths.Vector;
-
 import java.util.Arrays;
 
 /**
- * Snake entity that damages the player if ran into. Can be stationary or patrol back and forth between two points.
+ * Snake entity that patrols back and forth between two points. The snake is able to be killed by the player if player
+ * jumps on the snake's head. Kills player the player if it comes into contact with the player.
+ * @author David Hack
+ * @author Alex Mitchell
  */
 public class SnakeEntity extends BasicEnemyEntity implements Enemy {
 
+    // Constants
     private final static float WIDTH = 0.7f;
     private final static float HEIGHT = 1.5f;
-    private final static int RATE = 200;
+    private final static int RATE = 200; // Animation rate
 
+    /**
+     * Constructs a snake entity that moves until it detects a collision or no block underneath it.
+     * @param location The initial position.
+     */
     public SnakeEntity(Vector location){
         super(location, new Vector(WIDTH, HEIGHT));
-        AnimatedSprite as = new SlimeSprite(new Vector(WIDTH, HEIGHT), RATE);
+        AnimatedSprite as = new SnakeSprite(new Vector(WIDTH, HEIGHT), RATE);
         super.setSprite(as);
     }
 
-    private class SlimeSprite extends AnimatedSprite{
-        private SlimeSprite(Vector dimensions, int frameTick){
+    /**
+     * This class handles the appearance of the Snake Sprite.
+     * @author David Hack
+     */
+    private class SnakeSprite extends AnimatedSprite{
+        private SnakeSprite(Vector dimensions, int frameTick){
             super(new Vector(0,0), dimensions, frameTick);
-
             String state = "normal";
             String[] resources = new String[] {
                     "long-slime",
                     "long-slime2"
             };
             addResources(state, Arrays.asList(resources));
-
             setState("normal");
         }
     }
