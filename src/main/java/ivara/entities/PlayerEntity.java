50e7ed612d6ee3e2dee35fc435d7cd71defa83b1
Alex <alexjfmitchell@gmail.com>
2017-10-16 15:20:45 +1300

    Documented PlayerEntity

--- src/main/java/ivara/entities/PlayerEntity.java
+++ src/main/java/ivara/entities/PlayerEntity.java
@@ -13,25 +13,22 @@ import physics.PhysicProperties;
 import java.util.Arrays;
 
 /**
- * This class handles the entity of the player within the game
- *
+ * This class handles the behaviour of the PlayerEntity in the game.
  * @author Alex Mitchell
  * @author David Hack
  */
 public class PlayerEntity extends GameEntity {
 
-    //Dimensions
+    // Constants
     public static final float WIDTH = 1f;
     public static final float HEIGHT = 1.5f;
 
-    private static final float widthOff = 0.35f;
-    private static final float heightOff = 0.3f;
+    private static final float WIDTH_OFF = 0.35f;
+    private static final float HEIGHT_OFF = 0.3f;
 
-    private static final float jumpSensorHeight = 0.15f;
-    private static final float jumpSensorExtra = 0.01f;
-    private static final float antiWallRun = 0.05f; //0 for wall running
-
-    public int coinsCollected = 0;
+    private static final float JUMP_SENSOR_HEIGHT = 0.15f;
+    private static final float JUMP_SENSOR_EXTRA = 0.01f;
+    private static final float ANTI_WALL_RUN = 0.05f; // 0 for wall running
 
     //PLAYER STATES
     public final static String WALK_RIGHT = "walk-right";
@@ -43,6 +40,8 @@ public class PlayerEntity extends GameEntity {
     public final static String DUCK_RIGHT = "duck-right";
     public final static String DUCK_LEFT = "duck-left";
 
+    public int coinsCollected = 0;
+
     /**
      * Creates a PlayerEntity at a given position
      *
@@ -52,57 +51,53 @@ public class PlayerEntity extends GameEntity {
     public PlayerEntity(float x, float y) {
         super(new Vector(x, y));
 
-        //Velocity---
+        // Velocity
         VelocityComponent v = new VelocityComponent(this);
         addComponent(v);
 
-        //Sprites---
+        // Sprites
         SpriteComponent sc = new SpriteComponent(this);
-        //sc.add(new ResourceID("player"), new Vector(WIDTH, HEIGHT));
         PlayerSprite playerSprite = new PlayerSprite(new Vector(0,0), new Vector(WIDTH, HEIGHT), 160);
         sc.add(playerSprite);
         addComponent(sc);
 
-        //Collider---
-        Vector cTopLeft = new Vector(widthOff/2, heightOff);
-        Vector cDimensions = new Vector(WIDTH -widthOff, HEIGHT -heightOff);
+        // Collider
+        Vector cTopLeft = new Vector(WIDTH_OFF/2, HEIGHT_OFF);
+        Vector cDimensions = new Vector(WIDTH -WIDTH_OFF, HEIGHT -HEIGHT_OFF);
         addComponent(new ColliderComponent(this, new AABBCollider(AABBCollider.MIN_DIM, cTopLeft, cDimensions)));
 
-        //Layer---
+        // Layer
         addComponent(new RenderComponent(this, 999999999));
 
-        //Physics---
+        // Physics
         addComponent(new PhysicsComponent(this, new PhysicProperties(1, PhysicProperties.Type.DYNAMIC)));
 
-        //Sensors---
-        //AABB for the sensor
-        //FOR WALL RUNNING THIS SENSOR NEEDS TO BE THE SAME WIDTH AS THE COLLIDER, BIT SMALLER FOR NO WALL RUNNING
-        Vector sTopLeft = new Vector(widthOff/2 + antiWallRun, HEIGHT -jumpSensorHeight);
-        Vector sDimensions = new Vector(WIDTH -widthOff - antiWallRun*2, jumpSensorHeight + jumpSensorExtra);
+        // Sensors
+        Vector sTopLeft = new Vector(WIDTH_OFF/2 + ANTI_WALL_RUN, HEIGHT -JUMP_SENSOR_HEIGHT);
+        Vector sDimensions = new Vector(WIDTH -WIDTH_OFF - ANTI_WALL_RUN*2, JUMP_SENSOR_HEIGHT + JUMP_SENSOR_EXTRA);
         AABBCollider ab = new AABBCollider(AABBCollider.MIN_DIM, sTopLeft, sDimensions);
         Sensor bottomSensor = new Sensor(ab);
-
-        //This sensor goes around whole player except the jump sensor
-        cDimensions.y = cDimensions.y - jumpSensorHeight;
+        cDimensions.y = cDimensions.y - JUMP_SENSOR_HEIGHT;
         ab = new AABBCollider(AABBCollider.MIN_DIM, cTopLeft, cDimensions);
         Sensor enemySensor = new Sensor(ab);
-
         addComponent(new SensorComponent(this, new Sensor[]{bottomSensor, enemySensor}));
-
-        //Enable Listening for Sensor Events
         addComponent(new SensorHandlerComponent(this));
 
-        //Scripts---
+        // Scripts
         PlayerScript pc = new PlayerScript(playerSprite, bottomSensor, enemySensor);
         CameraScript cs = new CameraScript(this, new Vector(WIDTH /2, HEIGHT /2));
-
         ScriptComponent scriptComponent = new ScriptComponent(this);
         scriptComponent.add(pc);
         scriptComponent.add(cs);
-
         addComponent(scriptComponent);
     }
 
+    /**
+     * The PlayerSprite handles the current sprite displayed for the player.
+     * This changes based upon the player's current state.
+     * @author Will Pearson
+     * @author David Hack
+     */
     public class PlayerSprite extends AnimatedSprite {
 
         /**
