826f60e06573238eb3c733c114a4651759ad77b0
Alex <alexjfmitchell@gmail.com>
2017-10-16 14:58:45 +1300

    Added documentation to LinePlatformFactory

--- src/main/java/ivara/entities/LinePlatformFactory.java
+++ src/main/java/ivara/entities/LinePlatformFactory.java
@@ -1,13 +1,7 @@
 package ivara.entities;
 
-
-
 import core.entity.GameEntity;
-import ivara.entities.PlatformEntity;
-import maths.*;
 import maths.Vector;
-import scew.Entity;
-
 import java.util.*;
 
 /**
@@ -20,17 +14,42 @@ public final class LinePlatformFactory {
 
     private static boolean vege = true;
 
-//    public static Collection<GameEntity> linePlatforms(int x0, int y0, int x1, int y1) {
-
+    /**
+     * Constructs a collection of game entities that creates a line from point a to b.
+     * Vegetation is on by default with this constructor.
+     * @param x0 Starting x position.
+     * @param y0 Starting y position.
+     * @param x1 Ending x position.
+     * @param y1 Ending y position.
+     * @return A collection of GameEntities
+     */
     public static Collection<GameEntity> line(int x0, int y0, int x1, int y1) {
         return plotLine(x0,y0,x1,y1);
     }
 
+    /**
+     * Constructs a collection of game entities that creates a line from point a to b.
+     * Vegetation can be turned off with this constructor.
+     * @param x0 Starting x position.
+     * @param y0 Starting y position.
+     * @param x1 Ending x position.
+     * @param y1 Ending y position.
+     * @param veg Whether there is vegetation on the platform
+     * @return A collection of GameEntities
+     */
     public static Collection<GameEntity> line(int x0, int y0, int x1, int y1, boolean veg) {
         vege = veg;
         return plotLine(x0,y0,x1,y1);
     }
 
+    /**
+     * Plots the line given by the constructors and delegates further work to sub methods.
+     * @param x0 Starting x position.
+     * @param y0 Starting y position.
+     * @param x1 Ending x position.
+     * @param y1 Ending y position.
+     * @return A collection of GameEntities
+     */
     private static Collection<GameEntity> plotLine(int x0, int y0, int x1, int y1){
         int dx = x1 - x0;
         int dy = y1 - y0;
@@ -45,7 +64,6 @@ public final class LinePlatformFactory {
                 if (dx >= -dy) {
                     return plot3(x0,y0,x1,y1);
                 } else {
-                    //return plot4(x0,y0,x1,y1);
                     return plot4(x1,y1,x0,y0);
                 }
             }
@@ -54,7 +72,6 @@ public final class LinePlatformFactory {
                 if (-dx >= dy) {
                     return plot3(x1,y1,x0,y0);
                 } else {
-                    //return plot4(x1,y1,x0,y0);
                     return plot4(x0,y0,x1,y1);
                 }
             } else {
@@ -67,13 +84,15 @@ public final class LinePlatformFactory {
         }
     }
 
-//    private static GameEntity platformType(int x, int y, int n, boolean isVertical) {
-//        assert (n > 0);
-//        //if (n == 1)
-//
-//            return new BasicBlockEntity(x, y, "dirt");
-//    }
-        private static GameEntity platformType(int x, int y, int n, boolean isVertical) {
+    /**
+     * Constructs the necessary platform dependant on the input.
+     * @param x The x position of the first block.
+     * @param y The y position of the first block.
+     * @param n The number of blocks.
+     * @param isVertical Whether the block is vertical.
+     * @return A Platform with these constraints taken into consideration.
+     */
+    private static GameEntity platformType(int x, int y, int n, boolean isVertical) {
         assert(n > 0);
         if (n == 1)
             return new PlatformEntity(new Vector(x, y), vege);
@@ -83,11 +102,9 @@ public final class LinePlatformFactory {
             return new PlatformEntity(new Vector(x,y), n, false, vege);
     }
 
+    // Delegation of work into sub sections of the platform
 
-//    private static Collection<GameEntity> plot1(int x0, int y0, int x1, int y1) {
-//        Collection<GameEntity> platforms = new HashSet<>();
-//
-        private static Collection<GameEntity> plot1(int x0, int y0, int x1, int y1) {
+    private static Collection<GameEntity> plot1(int x0, int y0, int x1, int y1) {
         Collection<GameEntity> platforms = new HashSet<>();
         int dy = y1 - y0, d2y = dy+dy;
         int d2x = 2*(x1 - x0);
@@ -114,13 +131,6 @@ public final class LinePlatformFactory {
 
         return platforms;
     }
-
-//    private static Collection<GameEntity> plot2(int x0, int y0, int x1, int y1) {
-//        Collection<GameEntity> platforms = new HashSet<>();
-//        if (n != 0)
-//            platforms.add(platformType(x, originY, n, true));
-//        return platforms;
-//    }
 
     private static Collection<GameEntity> plot2(int x0, int y0, int x1, int y1) {
         Collection<GameEntity> platforms = new HashSet<>();
@@ -149,13 +159,6 @@ public final class LinePlatformFactory {
         return platforms;
     }
 
-//    private static Collection<GameEntity> plot3(int x0, int y0, int x1, int y1) {
-//        Collection<GameEntity> platforms = new HashSet<>();
-//        if (n != 0)
-//            platforms.add(platformType(originX, y, n, false));
-//        return platforms;
-//    }
-
     private static Collection<GameEntity> plot3(int x0, int y0, int x1, int y1) {
         Collection<GameEntity> platforms = new HashSet<>();
         int dx = x1 - x0, d2x = dx+dx;
@@ -182,13 +185,6 @@ public final class LinePlatformFactory {
         }
         return platforms;
     }
-
-//    private static Collection<GameEntity> plot4(int x0, int y0, int x1, int y1) {
-//        Collection<GameEntity> platforms = new HashSet<>();
-//        if (n != 0)
-//            platforms.add(platformType(originX, y, n, false));
-//        return platforms;
-//    }
 
     private static Collection<GameEntity> plot4(int x0, int y0, int x1, int y1) {
         Collection<GameEntity> platforms = new HashSet<>();
