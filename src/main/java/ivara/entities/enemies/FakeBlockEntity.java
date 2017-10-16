c2c8a7f802b68345b66a1bd216a3d38371a215b9
Alex <alexjfmitchell@gmail.com>
2017-10-16 13:23:25 +1300

    Documented FakeBlockEntity

--- src/main/java/ivara/entities/enemies/FakeBlockEntity.java
+++ src/main/java/ivara/entities/enemies/FakeBlockEntity.java
@@ -14,61 +14,68 @@ import physics.PhysicProperties;
 import java.util.Arrays;
 
 /**
- * Created by David Hack Local on 12-Oct-17.
+ * This class handles the creation and actions of a FakeBlockEntity.
+ * The fake block will fall if a player comes into contact with the top.
+ * @author David Hack
  */
 public class FakeBlockEntity extends GameEntity{
 
+    // Constants
     private final static float WIDTH = 1f;
     private final static float HEIGHT = 1f;
+    private final static float SENSOR_PADDING = 0.05f;
+    private final static float SENSOR_HEIGHT = 0.1f;
 
-    private float sensorPadding = 0.05f;
-    private float sensorHeight = 0.1f;
-
-    private Vector intialLoc;
-
-    private AnimatedSprite fbs;
+    private final Vector intialLoc;
+    private final AnimatedSprite fbs;
 
     public FakeBlockEntity(Vector transform){
         super(transform);
         intialLoc = new Vector(transform);
         Vector dimension = new Vector(WIDTH, HEIGHT);
 
-        //Velocity---
+        // Velocity
         VelocityComponent v = new VelocityComponent(this);
         v.set(new Vector(0f, 0f));
         addComponent(v);
 
-        //Layer---
+        // Layer
         addComponent(new RenderComponent(this, 1000));
 
-        //Collider--
+        // Collider
         Vector topLeft = new Vector(0,0);
         addComponent(new ColliderComponent(this, new AABBCollider(AABBCollider.MIN_DIM, topLeft, dimension)));
 
-        //Sprite---
+        // Sprite
         SpriteComponent sc = new SpriteComponent(this);
         fbs = new FakeBlockSprite(dimension);
         sc.add(fbs);
         addComponent(sc);
 
-        //Sensors---
-        Vector SensorTopLeft = new Vector(sensorPadding, 0);
-        Vector sensorDimension = new Vector(dimension.x-sensorPadding*2, sensorHeight);
+        // Sensors
+        Vector SensorTopLeft = new Vector(SENSOR_PADDING, 0);
+        Vector sensorDimension = new Vector(dimension.x-SENSOR_PADDING*2, SENSOR_HEIGHT);
         Sensor top = new Sensor(new AABBCollider(AABBCollider.MIN_DIM, SensorTopLeft, sensorDimension));
-
-        SensorTopLeft = new Vector(sensorPadding, dimension.y-sensorHeight);
+        SensorTopLeft = new Vector(SENSOR_PADDING, dimension.y-SENSOR_HEIGHT);
         Sensor bot = new Sensor(new AABBCollider(AABBCollider.MIN_DIM, SensorTopLeft, sensorDimension));
-
         addComponent(new SensorComponent(this, new Sensor[]{top, bot}));
 
-        //Enable Listening for Sensor Events
+        // Enable Listening for Sensor Events
         addComponent(new SensorHandlerComponent(this));
 
-        //Scripts---
+        // Scripts
         addComponent(new ScriptComponent(this, new FakeBlockScript(top, bot)));
     }
 
+    /**
+     * Handles the Sprite displayed by the FakeBlockEntity.
+     * @author David Hack
+     */
     private class FakeBlockSprite extends AnimatedSprite {
+        /**
+         * Constructs a new FakeBlockSprite that changes sprites dependant on the current state of the entity.
+         * @param dimensions The size.
+         */
         private FakeBlockSprite(Vector dimensions){
             super(new Vector(0,0), dimensions, 10000);
 
@@ -87,15 +94,26 @@ public class FakeBlockEntity extends GameEntity{
         }
     }
 
+    /**
+     * A FakeBlockScript handles the behaviour of the FakeBlock entity.
+     * @author David Hack
+     */
     private class FakeBlockScript implements Script {
 
+        // Constants
         private static final int FALL_DELAY = 500;
         private static final int REMOVE_DELAY = 1000;
 
-        Sensor top;
-        Sensor bot;
-        boolean alive;
+        private Sensor top;
+        private Sensor bot;
+        private boolean alive;
 
+        /**
+         * Constructs a FakeBlockScript with the two sensors of the entity.
+         * Sets the alive flag to true. The sprite updates on this flag.
+         * @param top The top sensor.
+         * @param bot The bottom sensor.
+         */
         public FakeBlockScript(Sensor top, Sensor bot){
             this.top = top;
             this.bot = bot;
@@ -107,7 +125,7 @@ public class FakeBlockEntity extends GameEntity{
             SensorHandler sensorHandler = entity.get(SensorHandlerComponent.class).get().getSensorHandler();
 
             if(sensorHandler.isActive(top) && alive){
-                if(sensorHandler.getActivatingEntities(top).stream().anyMatch((e) -> e instanceof PlayerEntity)) {
+                if(sensorHandler.getActivatingEntities(top).stream().anyMatch((e) -> e instanceof PlayerEntity)) { // If the player hits the top of a block, the block dies.
                     alive = false;
 
                     fbs.setState("dead");
@@ -126,6 +144,9 @@ public class FakeBlockEntity extends GameEntity{
             }
         }
 
+        /**
+         * Removes the block from the scene.
+         */
         public void doRemove(){
             if(alive) return;
             getScene().removeEntity(FakeBlockEntity.this);
