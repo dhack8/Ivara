8768afe70b8b11f43a32471e2a0c839bf864b19f
David Hack <hackdavi@ecs.vuw.ac.nz>
2019-10-19 15:21:13 +1300

    Remove old levels more work on level map

--- src/main/java/ivara/entities/LevelIconEntity.java
+++ src/main/java/ivara/entities/LevelIconEntity.java
@@ -1,19 +1,69 @@
 package ivara.entities;
 
+import core.Script;
+import core.components.ScriptComponent;
+import core.components.SpriteComponent;
 import core.entity.GameEntity;
+import core.input.Constants;
+import core.input.InputHandler;
+import core.scene.SceneUtils;
+import core.struct.ResourceID;
+import core.struct.Sprite;
 import ivara.scenes.Level;
 import maths.Vector;
+import physics.AABBCollider;
+import physics.Collider;
+import physics.CollisionUtil;
 
 public class LevelIconEntity extends GameEntity {
 
-    Level level;
-    Level preReqLevel;
+    private static final String AVAILABLE = "available";
+    private static final String UNAVAILABLE = "unavailable";
+    private static final String COMPLETED = "completed";
+
+    private static final float WIDTH = 0.7f;
+    private static final Vector SIZE = new Vector(WIDTH, WIDTH);
+
+    private Level level;
+    private Level preReqLevel;
+    private String state = UNAVAILABLE;
+    private Collider collider;
 
     public LevelIconEntity(Vector transform, Level level, Level preReqLevel) {
         super(transform);
         this.level = level;
         this.preReqLevel = preReqLevel;
+        if(preReqLevel == null) this.state = AVAILABLE;
+        this.collider = new AABBCollider(AABBCollider.MIN_DIM, new Vector(-WIDTH/2, -WIDTH/2), SIZE);
+
+        // Sprite
+        SpriteComponent sc = new SpriteComponent(this);
+        sc.add(new Sprite(new ResourceID("level-icon-" + state), new Vector(-WIDTH/2, -WIDTH/2), SIZE));
+        addComponent(sc);
+
+        // Scripts
+        ScriptComponent scriptComponent = new ScriptComponent(this);
+        scriptComponent.add(new Script() {
+            @Override
+            public void update(int dt, GameEntity entity) {
+                InputHandler.InputFrame input = getInput();
+
+                if (input.isMouseReleased(Constants.LEFT_MOUSE)) {
+                    if (isInIcon(input.getMousePosition())) {
+                        iconClicked();
+                    }
+                }
+            }
+        });
+        addComponent(scriptComponent);
     }
 
+    private void iconClicked() {
+        System.out.println("Icon clicked");
+    }
 
+    private boolean isInIcon(Vector position) {
+        Collider tC = SceneUtils.colliderToWorld(collider, this);
+        return CollisionUtil.contains(tC, position);
+    }
 }
--- src/main/java/ivara/scenes/LevelMap.java
+++ src/main/java/ivara/scenes/LevelMap.java
@@ -4,6 +4,8 @@ import core.scene.Scene;
 import core.struct.Camera;
 import core.struct.ResourceID;
 import ivara.entities.BackgroundEntity;
+import ivara.entities.LevelIconEntity;
+import maths.Vector;
 
 public class LevelMap extends Scene {
 
@@ -11,6 +13,21 @@ public class LevelMap extends Scene {
     public void initialize() {
         Camera c = new Camera();
         setCamera(c);
+
+        // Grass
+        addEntity(new LevelIconEntity(new Vector(7.275f, 8.4355f), new Level3(), null));
+
+        // Snow
+        addEntity(new LevelIconEntity(new Vector(11.7625f, 6.1625f), new Level3(), null));
+
+        // Alien
+        addEntity(new LevelIconEntity(new Vector(15.125f, 9.5375f), new Level3(), null));
+
+        // Alien: Top path
+
+
+        // Desert
+        addEntity(new LevelIconEntity(new Vector(10.65f, 11.8f), new Level3(), null));
 
         addEntity(new BackgroundEntity(new ResourceID("background-sea")));
         addEntity(new BackgroundEntity(new ResourceID("map")));
