c9a7751e3a5fd6e978f8e73deeb3296a1475c64f
taitkris <taitkris@myvuw.ac.nz>
2019-10-21 15:48:16 +1300

    Can purchase and select characters.

--- src/main/java/ivara/scenes/Store.java
+++ src/main/java/ivara/scenes/Store.java
@@ -8,12 +8,16 @@ import core.struct.ResourceID;
 import core.struct.Sprite;
 import core.struct.Text;
 import ivara.entities.*;
+import ivara.entities.BasicTextEntity;
+import ivara.entities.UIEntity;
+import ivara.entities.UIListener;
 import maths.Vector;
 import physics.AABBCollider;
 
-import javax.lang.model.type.ArrayType;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Kristen Tait on 1/10/2019.
@@ -34,28 +38,37 @@ public class Store extends Scene{
     private final float YPOS = 6; // The Y position to start drawing the line of buttons from
 
     private boolean confirmation = false;
-    private String playSelectedCharacter = "Pablo";
-    private String selectedCharacter = "Pablo";
-    private BasicTextEntity confirmationText = null;
-    private UIEntity yesButton = null;
-    private UIEntity noButton = null;
+    private String playSelectedCharacter;
+    private String selectedCharacter;
+    private BasicTextEntity confirmationText;
+    private UIEntity yesButton;
+    private UIEntity noButton;
+    private BasicTextEntity selectText;
 
-    private List<String> purchasedCharacters = new ArrayList<>();
+    private List<String> purchasedCharacters;
+    private Map<String, BasicTextEntity> characterCostMap;
+
 
     @Override
     public void initialize() {
 
+        this.characterCostMap = new HashMap<>();
+        this.purchasedCharacters = new ArrayList<>();
+
+        this.purchasedCharacters.add("Pablo");
+        this.selectedCharacter = "Pablo";
+        this.playSelectedCharacter = "Pablo";
+
         Camera c = new Camera();
         setCamera(c);
         Vector cDimensions = c.dimensions;
-        //TODO: uncommenting this also causes big red screen
-        //purchasedCharacters.add("Pablo");
 
         addEntity(new BasicTextEntity(new Vector(14f, 1.3f), new Text(40, "Store")));
         addEntity(new BasicTextEntity(new Vector(9f, 2f), new Text(20, "Buy one of Pablo's friends or select purchased character to play with!")));
+        selectText = new BasicTextEntity(new Vector(10f, 2.5f), new Text(20, "Currently playing as: Pablo"));
+        addEntity(selectText);
 
-        //TODO: this isn't showing up and I don't know why
-        addEntity(new StoreCoinEntity(new Vector(0f,0f)));
+        addEntity(new StoreCoinEntity(new Vector(50f,50f)));
 
         float leftoverX = cDimensions.x - (NUM_BUTTONS*BUTTON_WIDTH) - XMARGIN_LEFT - XMARGIN_RIGHT;
         float btnSpaceX = leftoverX/(NUM_BUTTONS+1);
@@ -64,23 +77,23 @@ public class Store extends Scene{
         addBackButton();
 
         addItemButton("pablo",
-                (UIListener) () -> showConfirmation("Pablo"),btnSpaceX, btnCount++, "Pablo", 0);
+                (UIListener) () -> showConfirmation("Pablo", 0),btnSpaceX, btnCount++, "Pablo", 0);
 
         //--- Female button
         addItemButton("female",
-                (UIListener) () -> showConfirmation("Female Player"),btnSpaceX, btnCount++, "Female Player", 20);
+                (UIListener) () -> showConfirmation("Stacey", 20),btnSpaceX, btnCount++, "Stacey", 20);
 
         //--- Male button
         addItemButton("adventurer",
-                (UIListener) () -> showConfirmation("Adventurer"),btnSpaceX, btnCount++, "Adventurer", 50);
+                (UIListener) () -> showConfirmation("Adventurer", 50),btnSpaceX, btnCount++, "Adventurer", 50);
 
         //--- Adventurer button
         addItemButton("male",
-                (UIListener) () -> showConfirmation("Male Player"),btnSpaceX, btnCount++, "Male Player", 20);
+                (UIListener) () -> showConfirmation("Nigel", 20),btnSpaceX, btnCount++, "Nigel", 20);
 
         //--- Zombie button
         addItemButton("zombie",
-                (UIListener) () -> showConfirmation("Zombie"),btnSpaceX, btnCount++, "Zombie", 100);
+                (UIListener) () -> showConfirmation("Zombie", 100),btnSpaceX, btnCount++, "Zombie", 100);
 
         addEntity(new BackgroundEntity(new ResourceID("background-sunrise")));
     }
@@ -97,27 +110,28 @@ public class Store extends Scene{
 
     private void addItemButton(String resourceID, UIListener buttonEvent, float btnSpaceX, int btnCount, String itemName, int cost){
 
-        addEntity(new BasicTextEntity(new Vector(XMARGIN_LEFT + btnSpaceX + (btnCount*(BUTTON_WIDTH + btnSpaceX)+2.7f),YPOS), new Text(20, itemName)));
-
-        //TODO: Big red screen is showing up for these. Trying to show selected character and add characters to purchased after they've been purchased.
-        //if(playSelectedCharacter.equals(itemName))
-        //   addEntity(new BasicTextEntity(new Vector(XMARGIN_LEFT + btnSpaceX + (btnCount*(BUTTON_WIDTH + btnSpaceX)+2.7f),YPOS+0.5f),
-        //            new Text(20, "Selected")));
-        //if(!purchasedCharacters.contains(itemName))
-        //    addEntity(new BasicTextEntity(new Vector(XMARGIN_LEFT + btnSpaceX + (btnCount*(BUTTON_WIDTH + btnSpaceX)+2.7f),YPOS+0.5f),
-        //            new Text(20, cost + " Coins")));
-
+        BasicTextEntity itemText = new BasicTextEntity(new Vector(XMARGIN_LEFT + btnSpaceX + (btnCount*(BUTTON_WIDTH + btnSpaceX)+2.7f),YPOS), new Text(20, itemName));
+        BasicTextEntity costText = new BasicTextEntity(new Vector(XMARGIN_LEFT + btnSpaceX + (btnCount*(BUTTON_WIDTH + btnSpaceX)+2.7f),YPOS+0.5f),
+                new Text(20, cost + " Coins"));
         UIEntity button = new UIEntity(new Vector(XMARGIN_LEFT + btnSpaceX + (btnCount*(BUTTON_WIDTH + btnSpaceX)),YPOS),
                 new Sprite(new ResourceID(resourceID), new Vector(0, 0), new Vector(BUTTON_WIDTH, BUTTON_HEIGHT)),
                 new AABBCollider(AABBCollider.MIN_DIM, new Vector(0, 0), new Vector(BUTTON_WIDTH, BUTTON_HEIGHT)));
-        button.addListener(buttonEvent);
+        addEntity(itemText);
+        characterCostMap.put(itemName, costText);
 
+        if(playSelectedCharacter.equals(itemName)){
+            addEntity(selectText);
+        }
+        if(!purchasedCharacters.contains(itemName)) addEntity(costText);
+
+
+        button.addListener(buttonEvent);
         addEntity(button);
     }
 
-    private void showConfirmation(String itemName) {
+    private void showConfirmation(String itemName, int cost) {
         removeConfirmation();
-        if (!confirmation || !selectedCharacter.equals(itemName)) {
+        if (!confirmation && !selectedCharacter.equals(itemName) && !purchasedCharacters.contains(itemName)) {
             confirmation = true;
             confirmationText = new BasicTextEntity(new Vector(14f, 3f), new Text(20, "Purchase " + itemName + "?"));
 
@@ -126,7 +140,7 @@ public class Store extends Scene{
                     new AABBCollider(AABBCollider.MIN_DIM, new Vector(0, 0), new Vector(YESNO_WIDTH/2f, YESNO_HEIGHT/2f)));
             yesButton.addListener((UIListener) () -> {
                 //TODO: uncommenting this also causes big red screen
-                //purchase(itemName);
+                purchase(itemName, cost);
                 removeConfirmation();
             });
 
@@ -139,7 +153,9 @@ public class Store extends Scene{
             addEntity(yesButton);
             addEntity(noButton);
 
-        } else removeConfirmation();
+        } else if(!playSelectedCharacter.equals(itemName)){
+            select(itemName);
+        }
     }
 
     private void removeConfirmation(){
@@ -149,14 +165,19 @@ public class Store extends Scene{
         removeEntity(noButton);
     }
 
-    private void purchase(String itemName){
+    private void purchase(String itemName, int cost){
         //TODO: take off coins from player.
-        //PURCHASE CHARACTER
         purchasedCharacters.add(itemName);
+        removeEntity(characterCostMap.get(itemName));
+        // PlayerEntity.spendCoins(cost);
     }
 
     private void select(String itemName){
-        //TODO: change game character to blah
-        selectedCharacter = itemName;
+        playSelectedCharacter = itemName;
+        removeEntity(selectText);
+        selectText = new BasicTextEntity(new Vector(10f, 2.5f),
+                new Text(20, "Currently playing as: " + itemName));
+        addEntity(selectText);
+        //TODO: change to play as selected character
     }
 }
