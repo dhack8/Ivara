0e460f85d55145afc2504284dd7f783bd1cb695b
Will Pearson <pearsowill@ecs.vuw.ac.nz>
2019-10-23 16:13:39 +1300

    Added base level for DoubleJump2

--- src/main/java/ivara/scenes/boots/DoubleJump1.java
+++ src/main/java/ivara/scenes/boots/DoubleJump1.java
@@ -31,7 +31,8 @@ public class DoubleJump1 extends Level {
         addEntity(new CheckpointEntity(56, 29));
 
         // Flag
-        addEntity(new LevelEndEntity(53, 16));
+//        addEntity(new LevelEndEntity(53, 16));
+        addEntity(new LevelEndEntity(1, 8));
 
         // Platforms
         addEntity(new PlatformEntity("alien", new Vector(61,1),21,true));
